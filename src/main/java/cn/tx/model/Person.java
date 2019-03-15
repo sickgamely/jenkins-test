9e2cf18e013eb633f92e065a89cc8011b7a34b2b
Administrator <admin@example.com>
2019-03-15 10:07:52 +0800

    first

---
+++ src/main/java/cn/tx/model/Person.java
@@ -0,0 +1,13 @@
+package cn.tx.model;
+
+public class Person {
+    private String name = "txjava";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ target/jenkins-test/index.jsp
@@ -0,0 +1,7 @@
+<%@ page import="cn.tx.model.Person" %>
+<html>
+<body>
+<h2>Hello Jenkins</h2>
+<h3><%=new Person().getName()%></h3>
+</body>
+</html>
